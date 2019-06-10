ba29024ed5360d394cb7f2ab65966be59a89581c
zc <zc2638@qq.com>
2019-06-10 13:34:00 +0800

    将接口请求改为异步

--- controller/admin/home.go
+++ controller/admin/home.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"strings"
 	"sync"
+	"time"
 )
 
 type HomeController struct{ controller.Base }
@@ -88,15 +89,22 @@ func (t *HomeController) IndustryData(c *gin.Context) {
 	| stats count by 应用对接优先级&output_mode=json`, // 应用优先级
 	}
 
-	uri := `https://192.168.5.85:8089/services/search/jobs/export?`
+	uri := "https://192.168.5.85:8089/services/search/jobs/export?"
 
+	// 因为确定请求内容位置，所以不会出现并发写入问题，可以直接用
 	var data = make([]interface{}, 3)
 
 	var wg sync.WaitGroup
 	for k, v := range vals {
 		wg.Add(1)
 		go func(u string, k int) {
-			b, err := curl.Get(u)
+			h := curl.HttpReq{
+				Url: u,
+				Header: map[string]string{
+					"Authorization": "Basic YWRtaW46YWx3YXlzYmVjb2Rpbmc=",
+				},
+			}
+			b, err := h.Get()
 			if err != nil {
 				t.Err(c, err.Error())
 				return
@@ -108,6 +116,7 @@ func (t *HomeController) IndustryData(c *gin.Context) {
 				if vv != "" {
 					if err := json.Unmarshal([]byte(vv), &source); err != nil {
 						logger.Info("Json Error:", err.Error())
+						wg.Done()
 						return
 					}
 					origin = append(origin, source)
@@ -137,21 +146,38 @@ func (t *HomeController) OperateData(c *gin.Context) {
 	}
 	uri := `http://192.168.170.18:9090/api/v1/query?query=esxi_cpu_usage`
 
+	var wg sync.WaitGroup
+	var cpuMap sync.Map
 	var cpuData = make(map[string]interface{})
 	for k, v := range ipList {
 		for _, vv := range v {
-			params := `{cluster="` + k + `",id="` + vv + `",exported_job="cpu_usage",instance="pushgateway",job="pushgateway"}`
-			b, err := curl.Get(uri + params)
-			var res dataStruct.CPUData
-			if err == nil {
-				if err := json.Unmarshal(b, &res); err == nil && res.Status == "success" && len(res.Data.Result) == 1 {
-					cpuData[vv] = res.Data.Result[0].Value
-					continue
+			wg.Add(1)
+			go func(cluster, ip string) {
+				params := `{cluster="` + cluster + `",id="` + ip + `",exported_job="cpu_usage",instance="pushgateway",job="pushgateway"}`
+				h := curl.HttpReq{
+					Url:     uri + params,
+					Timeout: time.Second * 5,
 				}
-			}
-			cpuData[vv] = []int{0, 0}
+				b, err := h.Get()
+
+				var res dataStruct.CPUData
+				if err == nil {
+					if err := json.Unmarshal(b, &res); err == nil && res.Status == "success" && len(res.Data.Result) == 1 {
+						cpuMap.Store(vv, res.Data.Result[0].Value)
+						wg.Done()
+						return
+					}
+				}
+				cpuMap.Store(vv, []int{0, 0})
+				wg.Done()
+			}(k, vv)
 		}
 	}
+
+	cpuMap.Range(func(k, v interface{}) bool {
+		cpuData[k.(string)] = v
+		return true
+	})
 
 	uriI := `http://192.168.170.18:9090/api/v1/query?query=ifHCInOctets{ifAlias="eth2",ifDescr="WAN2",ifIndex="16777218",ifName="WAN2",instance="192.168.1.1",job="snmp",tag="DaoCloud_Route"}`
 	uriO := `http://192.168.170.18:9090/api/v1/query?query=ifHCOutOctets{ifAlias="eth2",ifDescr="WAN2",ifIndex="16777218",ifName="WAN2",instance="192.168.1.1",job="snmp",tag="DaoCloud_Route"}`
@@ -159,22 +185,41 @@ func (t *HomeController) OperateData(c *gin.Context) {
 	var ioData = make([]interface{}, 2)
 	ioData[0] = []int{0, 0}
 	ioData[1] = []int{0, 0}
-
-	var vi dataStruct.IOData
-	bi, err := curl.Get(uriI)
-	if err == nil {
-		if err := json.Unmarshal(bi, &vi); err == nil && vi.Status == "success" && len(vi.Data.Result) == 1 {
-			ioData[0] = vi.Data.Result[0].Value
+	wg.Add(2)
+	go func() {
+		var vi dataStruct.IOData
+		h := curl.HttpReq{
+			Url:     uriI,
+			Timeout: time.Second * 5,
 		}
-	}
-
-	var vo dataStruct.IOData
-	bo, err := curl.Get(uriO)
-	if err == nil {
-		if err := json.Unmarshal(bo, &vo); err == nil && vo.Status == "success" && len(vo.Data.Result) == 1 {
-			ioData[1] = vo.Data.Result[0].Value
+		bi, err := h.Get()
+		if err == nil {
+			if err := json.Unmarshal(bi, &vi); err == nil && vi.Status == "success" && len(vi.Data.Result) == 1 {
+				ioData[0] = vi.Data.Result[0].Value
+				wg.Done()
+				return
+			}
 		}
-	}
+		wg.Done()
+	}()
+
+	go func() {
+		var vo dataStruct.IOData
+		h := curl.HttpReq{
+			Url:     uriO,
+			Timeout: time.Second * 5,
+		}
+		bo, err := h.Get()
+		if err == nil {
+			if err := json.Unmarshal(bo, &vo); err == nil && vo.Status == "success" && len(vo.Data.Result) == 1 {
+				ioData[1] = vo.Data.Result[0].Value
+				wg.Done()
+				return
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
 
 	t.Data(c, gin.H{
 		"cpu": cpuData,
@@ -187,41 +232,57 @@ func (t *HomeController) IOData(c *gin.Context) {
 	uriI := `http://192.168.170.18:9090/api/v1/query?query=ifHCInOctets{ifAlias="eth2",ifDescr="WAN2",ifIndex="16777218",ifName="WAN2",instance="192.168.1.1",job="snmp",tag="DaoCloud_Route"}[2h]&step=30s`
 	uriO := `http://192.168.170.18:9090/api/v1/query?query=ifHCOutOctets{ifAlias="eth2",ifDescr="WAN2",ifIndex="16777218",ifName="WAN2",instance="192.168.1.1",job="snmp",tag="DaoCloud_Route"}[2h]&step=30s`
 
-	bi, err := curl.Get(uriI)
-	if err != nil {
-		t.Err(c, "input获取错误")
+	var ioData = make(map[string]interface{})
+	ioData["input"] = nil
+	ioData["output"] = nil
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		h := curl.HttpReq{
+			Url:     uriI,
+			Timeout: time.Second * 5,
+		}
+		bi, err := h.Get()
+		if err == nil {
+			var vi dataStruct.IODataSet
+			if err := json.Unmarshal(bi, &vi); err == nil && vi.Status == "success" && len(vi.Data.Result) == 1 {
+				ioData["input"] = vi.Data.Result[0].Values
+				wg.Done()
+				return
+			}
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		h := curl.HttpReq{
+			Url:     uriO,
+			Timeout: time.Second * 5,
+		}
+		bo, err := h.Get()
+		if err == nil {
+			var vo dataStruct.IODataSet
+			if err := json.Unmarshal(bo, &vo); err == nil && vo.Status == "success" && len(vo.Data.Result) == 1 {
+				ioData["output"] = vo.Data.Result[0].Values
+				wg.Done()
+				return
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	if ioData["input"] == nil {
+		t.Err(c, "input数据抓取异常")
+		return
+	}
+	if ioData["output"] == nil {
+		t.Err(c, "output数据抓取异常")
 		return
 	}
 
-	var vi dataStruct.IODataSet
-	if err := json.Unmarshal(bi, &vi); err != nil {
-		t.Err(c, "input解析错误")
-		return
-	}
-
-	bo, err := curl.Get(uriO)
-	if err != nil {
-		t.Err(c, "output获取错误")
-		return
-	}
-
-	var vo dataStruct.IODataSet
-	if err := json.Unmarshal(bo, &vo); err != nil {
-		t.Err(c, "output解析错误")
-		return
-	}
-
-	var iData interface{}
-	var oData interface{}
-	if vi.Status == "success" && len(vi.Data.Result) == 1 {
-		iData = vi.Data.Result[0].Values
-	}
-	if vo.Status == "success" && len(vo.Data.Result) == 1 {
-		oData = vo.Data.Result[0].Values
-	}
-
-	t.Data(c, gin.H{
-		"input":  iData,
-		"output": oData,
-	})
+	t.Data(c, ioData)
 }
--- lib/curl/curl.go
+++ lib/curl/curl.go
@@ -1,6 +1,7 @@
 package curl
 
 import (
+	"bytes"
 	"crypto/tls"
 	"io/ioutil"
 	"net/http"
@@ -9,26 +10,86 @@ import (
 	"time"
 )
 
-func Get(uri string) ([]byte, error) {
+const (
+	METHOD_POST = "POST"
+	METHOD_GET  = "GET"
+)
+
+type M map[string]interface{}
+
+type HttpReq struct {
+	Url      string
+	Method   string
+	Header   map[string]string
+	Params   M
+	Body     []byte
+	CertFile string
+	KeyFile  string
+	Timeout  time.Duration
+}
+
+func (h *HttpReq) buildBody() {
+	if h.Body != nil {
+		return
+	}
+
+	var data string
+	for k, v := range h.Params {
+		if data != "" {
+			data += "&"
+		}
+		data += k + "=" + v.(string)
+	}
+
+	switch h.Method {
+	case METHOD_POST:
+		h.Body = []byte(data)
+		break
+	case METHOD_GET:
+		urlArr := strings.Split(h.Url, "?")
+		if len(urlArr) == 2 {
+			if data != "" {
+				urlArr[1] = urlArr[1] + "&" + data
+			}
+			//将GET请求的参数进行转义
+			h.Url = urlArr[0] + "?" + url.PathEscape(urlArr[1])
+		}
+		break
+	}
+}
+
+func (h *HttpReq) Do() ([]byte, error) {
 
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-
-	client := &http.Client{
+	if h.CertFile != "" {
+		cert, err := tls.LoadX509KeyPair(h.CertFile, h.KeyFile)
+		if err != nil {
+			return nil, err
+		}
+		tr.DisableCompression = true
+		tr.TLSClientConfig = &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
+	}
+	var client = &http.Client{
 		Transport: tr,
-		Timeout: 5 * time.Second, // 5s
+		Timeout:   h.Timeout,
+	}
+	h.buildBody()
+
+	req, err := http.NewRequest(h.Method, h.Url, bytes.NewReader(h.Body))
+	if err != nil {
+		return nil, err
 	}
 
-	urlArr := strings.Split(uri, "?")
-	if len(urlArr) == 2 {
-		//将GET请求的参数进行转义
-		uri = urlArr[0] + "?" + url.PathEscape(urlArr[1])
+	if h.Header != nil {
+		for k, v := range h.Header {
+			req.Header.Set(k, v)
+		}
 	}
-	var req *http.Request
-	req, _ = http.NewRequest("GET", uri, nil)
 
-	req.Header.Add("Authorization", "Basic YWRtaW46YWx3YXlzYmVjb2Rpbmc=")
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
@@ -36,4 +97,10 @@ func Get(uri string) ([]byte, error) {
 	defer resp.Body.Close()
 
 	return ioutil.ReadAll(resp.Body)
+}
+
+func (h *HttpReq) Get() ([]byte, error) {
+
+	h.Method = METHOD_GET
+	return h.Do()
 }
